68f9207b7fd8d2bbff86f1bafdadff0aa14cb5d5
jeremypatrickdahan <jeremydahan@protonmail.com>
2021-07-31 16:12:30 +0200

    wip

--- src/components/QuestionTypes/RateType.vue
+++ src/components/QuestionTypes/RateType.vue
@@ -3,18 +3,12 @@
     ref="input"
     v-bind:value="value"
     v-bind:required="question.required"
-    v-on:keydown="onKeyDown"
-    v-on:keyup="onChange({ target: { value: $event } })"
-    v-on:keyup.enter.prevent="onEnter"
-    v-on:keyup.tab.prevent="onEnter"
-    v-on:focus="setFocus"
-    v-on:blur="unsetFocus"
     v-on:change="onChange({ target: { value: $event } })"
-    v-bind:placeholder="placeholder"
   />
 </template>
 
 <script>
+// TODO : auto after click
 import BaseType from "./BaseType.vue";
 import { QuestionType } from "../../models/QuestionModel";
 import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
